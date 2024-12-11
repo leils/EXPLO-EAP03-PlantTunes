ddcc283a9883a629092d5b6ece8bc7fe061108b2
Leia Chang <leia.chang@exploratorium.edu>
2024-12-11 14:00:30 -0800

    arduino reports stable state of multiple pins

--- cap1188test/cap1188test.ino
+++ cap1188test/cap1188test.ino
@@ -1,93 +1,119 @@
-/*************************************************** 
+/***************************************************
   Plant Tunes - Single Channel
   Designed specifically to work with the CAP1188 sensor from Adafruit
   ----> https://www.adafruit.com/products/1602
 
  ****************************************************/
- 
+
 #include <Wire.h>
 #include <SPI.h>
 #include "Adafruit_CAP1188.h"
 
 // Reset Pin is used for I2C or SPI
-#define CAP1188_RESET  9
+#define CAP1188_RESET 9
 
 // CS pin is used for software or hardware SPI
-#define CAP1188_CS  10
+#define CAP1188_CS 10
 
-// These are defined for software SPI, for hardware SPI, check your 
+// These are defined for software SPI, for hardware SPI, check your
 // board's SPI pins in the Arduino documentation
-#define CAP1188_MOSI  11
-#define CAP1188_MISO  12
-#define CAP1188_CLK  13
+#define CAP1188_MOSI 11
+#define CAP1188_MISO 12
+#define CAP1188_CLK 13
 
 // Or...Use I2C, with reset pin
 Adafruit_CAP1188 cap = Adafruit_CAP1188(CAP1188_RESET);
 
-// Storage for last 5 readings of CAP pin 0 
-const int bufferSize = 5; // Size of the circular buffer
-int sensorReadings[bufferSize]; // Array to store the readings
-int currentIndex = 0; // Index to track the current position in the buffer
+const int NumCapPins = 4;
 
-void setup() {
+// Storage for last 5 readings of CAP pin 0
+const int bufferSize = 5;                   // Size of the buffer
+int sensorReadings[NumCapPins][bufferSize]; // Array to store the last 5 readings of each pin
+int stableReadings[NumCapPins] = { 0 };
+
+void setup()
+{
   Serial.begin(9600);
 
-  int cnt = 1000;     // Will wait for up to ~1 second for Serial to connect.
-  while (!Serial && cnt--) {
+  int cnt = 1000; // Will wait for up to ~1 second for Serial to connect.
+  while (!Serial && cnt--)
+  {
     delay(1);
   }
   Serial.println("CAP1188 test!");
 
-  if (!cap.begin()) {
+  if (!cap.begin())
+  {
     Serial.println("CAP1188 not found");
-    while (1);
+    while (1)
+      ;
   }
   Serial.println("CAP1188 found!");
 
   // Write sensitivity from 0x7f(1x) to 0x2f(32x, default)
-  // Referenced from https://forums.adafruit.com/viewtopic.php?t=55900 
-  cap.writeRegister(CAP1188_SENSITIVITY, 0x6F);  // 4x  sensitivity
-  Serial.print("Sensitivity: 0x");
-  Serial.println(cap.readRegister(CAP1188_SENSITIVITY), HEX);
+  // Referenced from https://forums.adafruit.com/viewtopic.php?t=55900
+  // cap.writeRegister(CAP1188_SENSITIVITY, 0x6F); // 4x  sensitivity
+  // Serial.print("Sensitivity: 0x");
+  // Serial.println(cap.readRegister(CAP1188_SENSITIVITY), HEX);
 
-  // Init all sensor readings to 0 
-  for (int i = 0; i < bufferSize; i++) {
-    sensorReadings[i] = 0;
+  // Init all sensor readings to 0
+  for (int p = 0; p < NumCapPins; p++)
+  {
+    for (int i = 0; i < bufferSize; i++)
+    {
+      sensorReadings[p][i] = 0;
+    }
   }
 }
 
-void loop() {
+void loop()
+{
   uint8_t touched = cap.touched();
 
-  if (touched & (1 << 7)) {
+  if (touched & (1 << 7))
+  {
     Serial.print("Sensitivity: 0x");
     Serial.println(cap.readRegister(CAP1188_SENSITIVITY), HEX);
   }
-  // Add the new reading to the buffer
-  addReading(touched);
-  reportStableState();
+  addReadings(touched); // Add the new reading to the buffer
+  updateStableState();  // Debounce readings
+
+  // Report current stable state via Serial
+  for (int i=0; i<NumCapPins; i++) {
+    Serial.print(stableReadings[i]); Serial.print(" ");
+  }
+  Serial.println();
+
   delay(50);
 }
 
-// Function to add a new reading using FIFO logic
-void addReading(int newValue) {
+
+// For each pin, adds the current sensor reading to the end of the readings buffer
+void addReadings(uint8_t touchedReadings)
+{
   // Shift all readings one position to the left
-  for (int i = 0; i < bufferSize - 1; i++) {
-    sensorReadings[i] = sensorReadings[i + 1];
+  for (int p = 0; p < NumCapPins; p++)
+  {
+    for (int i = 0; i < bufferSize - 1; i++)
+    {
+      sensorReadings[p][i] = sensorReadings[p][i + 1];
+    }
+
+    bool currentSensorTouched = (touchedReadings & (1 << p));
+    sensorReadings[p][bufferSize - 1] = currentSensorTouched;
   }
-  // Add the new value to the last position
-  sensorReadings[bufferSize - 1] = newValue;
 }
 
-int reportStableState() {
-  int sumTotal = 0;
-  for (int i=0; i<5; i++) {
-    sumTotal += sensorReadings[i];
+void updateStableState() {
+  for (int pin = 0; pin < NumCapPins; pin++) {
+    int readTotalForCurrentPin = 0;
+    for (int r = 0; r < bufferSize; r++) {
+      readTotalForCurrentPin += sensorReadings[pin][r];
+    }
+    if (readTotalForCurrentPin == 0) {  // pin has been low 
+      stableReadings[pin] = 0;
+    } else if (readTotalForCurrentPin == 5) {
+      stableReadings[pin] = 1;
+    }
   }
-  if (sumTotal == 0) {
-    Serial.println(0);
-  } else if (sumTotal == 5) {
-    Serial.println(1);
-  } 
 }
-
