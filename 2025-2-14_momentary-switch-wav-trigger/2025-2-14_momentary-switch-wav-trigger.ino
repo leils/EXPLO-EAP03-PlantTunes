0df0be9643c1868ebb85c55db76efa96525e0dea
Leia Chang <leia.chang@exploratorium.edu>
2025-02-18 16:56:13 -0800

    add delays to wTrig calls and poll for connection

--- 2025-2-14_momentary-switch-wav-trigger/2025-2-14_momentary-switch-wav-trigger.ino
+++ 2025-2-14_momentary-switch-wav-trigger/2025-2-14_momentary-switch-wav-trigger.ino
@@ -2,6 +2,7 @@
 #include <wavTrigger.h>
 
 wavTrigger wTrig;
+char gWTrigVersion[VERSION_STRING_LEN];
 
 #define CAP_POWER_PIN_1 10
 #define CAP_OUT_PIN_1 11
@@ -10,50 +11,72 @@ wavTrigger wTrig;
 #define BUTTON_PIN 12
 
 
-#define PIXEL_PIN 6 
-#define NUMPIXELS 30 
+#define PIXEL_PIN 6
+#define NUMPIXELS 30
+
 Adafruit_NeoPixel pixels(NUMPIXELS, PIXEL_PIN, NEO_RGB + NEO_KHZ800);
 
 byte lastCap1 = 0;
-byte lastCap2 = 0; 
+byte lastCap2 = 0;
 
 void setup() {
   Serial.begin(9600);
-  wTrig.start();
 
   pinMode(CAP_POWER_PIN_1, OUTPUT);
-  pinMode(CAP_OUT_PIN_1, INPUT_PULLDOWN);  
+  pinMode(CAP_OUT_PIN_1, INPUT_PULLDOWN);
   pinMode(CAP_POWER_PIN_2, OUTPUT);
   pinMode(CAP_OUT_PIN_2, INPUT_PULLDOWN);
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
 
-  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+  pixels.begin();  // INITIALIZE NeoPixel strip object (REQUIRED)
 
   digitalWrite(CAP_POWER_PIN_1, HIGH);
   digitalWrite(CAP_POWER_PIN_2, HIGH);
   digitalWrite(LED_BUILTIN, LOW);
 
-  delay(100);
-  wTrig.stopAllTracks();
+  while (!wTrig.getVersion(gWTrigVersion, VERSION_STRING_LEN)) {  //Use version get to indicate connectivity
+    Serial.println("Attempting to connect to WTrig ...");
+    delay(10);
+    wTrig.start();
+    delay(10);
+  }
+
+  Serial.println("WTrig successfully connected");
   delay(10);
-  wTrig.samplerateOffset(0);
 
   loadAndPlayTracksMuted();
 }
 
+void testAndConnectWTrig() {
+  while (!wTrig.getVersion(gWTrigVersion, VERSION_STRING_LEN)) {  //Use version get to indicate connectivity
+    Serial.println("Attempting to connect to WTrig ...");
+    delay(10);
+    wTrig.start();
+    delay(10);
+  }
+
+  Serial.println("WTrig successfully connected");
+  delay(10);
+}
+
 void loadAndPlayTracksMuted() {
+  // Create a clean slate with no playing tracks
+  wTrig.stopAllTracks();
+  delay(10);
+  wTrig.samplerateOffset(0);
+  delay(10);
   // Mark all tracks for looping, mute, and load them
-  for (int i=0; i < 4; i++ ){
+  for (int i = 0; i < 4; i++) {
     wTrig.trackLoop(i, true);
-    delay(10);    // The Wav Trigger can't handle consecutive commands via serial, needs some processing time 
+    delay(10);  // The Wav Trigger can't handle consecutive commands via serial, needs some processing time
     wTrig.trackGain(i, -70);
     delay(10);
     wTrig.trackLoad(i);
     delay(10);
-   }
+  }
 
-   wTrig.resumeAllInSync(); //Play all tracks
+  wTrig.resumeAllInSync();  //Play all tracks
 }
 
 
@@ -84,49 +107,52 @@ void loop() {
   // ---------- Catch rising/falling edges on cap sensors for sound volume
   // On a rising edge, raises track volume to 0 gain (normal volume)
   // On a falling edge, lowers track volume to -70 gain (mute)
-  if (cap1 > lastCap1) {  // rising edge 
+  if (cap1 > lastCap1) {  // rising edge
     Serial.println("cap1 rising");
     wTrig.trackFade(1, 0, 500, false);
-  } else if (cap1 < lastCap1) {   // falling edge 
+    delay(10);
+  } else if (cap1 < lastCap1) {  // falling edge
     Serial.println("Cap1 falling");
     wTrig.trackFade(1, -70, 500, false);
+    delay(10);
   }
 
-  if (cap2 > lastCap2) { // rising edge
+  if (cap2 > lastCap2) {  // rising edge
     Serial.println("Cap2 rising");
-
     wTrig.trackFade(2, 0, 500, false);
-  } else if (cap2 < lastCap2) {   // falling edge
+    delay(10);
+  } else if (cap2 < lastCap2) {  // falling edge
     Serial.println("Cap2 falling");
     wTrig.trackFade(2, -70, 500, false);
+    delay(10);
   }
 
-  // ---------- LED color handling 
+  // ---------- LED color handling
   // If any change has occurred since the last reading, change LED colors
-  if ((cap1 != lastCap1) || (cap2 != lastCap2) ) {
+  if ((cap1 != lastCap1) || (cap2 != lastCap2)) {
     if (cap1 && !cap2) {
       pixels.clear();
-      for(int i=NUMPIXELS-1; i>17; i--) {
-        pixels.setPixelColor(i, pixels.Color(100,100,0));
+      for (int i = NUMPIXELS - 1; i > 17; i--) {
+        pixels.setPixelColor(i, pixels.Color(100, 100, 0));
       }
     } else if (!cap1 && cap2) {
       pixels.clear();
-      for (int i=0; i<12; i++) {
-        pixels.setPixelColor(i, pixels.Color(100,100,0));
+      for (int i = 0; i < 12; i++) {
+        pixels.setPixelColor(i, pixels.Color(100, 100, 0));
       }
     } else if (cap1 && cap2) {
-      pixels.fill(pixels.Color(100,100,100));
-  
-     
+      pixels.fill(pixels.Color(100, 100, 100));
+
+
     } else {
-      pixels.clear(); // Set all pixel colors to 'off'
+      pixels.clear();  // Set all pixel colors to 'off'
     }
     pixels.show();
   }
 
 
   lastCap1 = cap1;
-  lastCap2 = cap2; 
-  
+  lastCap2 = cap2;
+
   delay(50);
 }
