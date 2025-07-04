d440c5e5c586a52d45751463243d2042824b722f
Leia Chang <leia.chang@exploratorium.edu>
2025-07-03 17:04:03 -0700

    standalone plant now shows lights based on touch, on/off at threshold 100

--- 2025-7-2_standalonePlant/2025-7-2_standalonePlant.ino
+++ 2025-7-2_standalonePlant/2025-7-2_standalonePlant.ino
@@ -36,35 +36,16 @@ void setup() {
 }
 
 void loop() {
-
-  if (Serial.available() > 0) {
-    String input = Serial.readStringUntil('\n');
-    input.trim(); // remove any trailing \r or whitespace
-
-    Serial.print("You said: ");
-    Serial.println(input);
-
-    if (input == "recal") {
-      Serial.println("Recalibrating to ambient");
-      cap.begin(0x5A, &Wire1);
-    } else if (input == "baseline") {
-      Serial.println(cap.baselineData(0));
-    }
+  // Serial.println(cap.baselineData(0));
+  Serial.println(cap.filteredData(0));
+  
+  if (cap.filteredData(0) < 100) {
+    pixels.fill(pixels.Color(100,100,30));
+    pixels.show();
+  } else {
+    pixels.clear();
+    pixels.show();
   }
 
-  else {
-    Serial.println(cap.baselineData(0));
-
-    delay(50);
-  }
-//   pixels.clear(); // Set all pixel colors to 'off'
-
-//   for(int i=0; i<NUMPIXELS; i++) { // For each pixel...
-
-//     pixels.setPixelColor(i, pixels.Color(100, 100, 30));
-
-//     pixels.show();   // Send the updated pixel colors to the hardware.
-
-//     delay(DELAYVAL); // Pause before next pass through loop
-//   }
+  delay(50);
 }
